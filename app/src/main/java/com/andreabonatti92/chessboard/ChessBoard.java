f643989ec689721055bfe4e277e04b3c865491e0
Andrea Bonatti <andreabonatti92@gmail.com>
2024-10-09 11:04:31 +0200

    Add the basic explanation of what is main idea inside the ChessBoard class

--- app/src/main/java/com/andreabonatti92/chessboard/ChessBoard.java
+++ app/src/main/java/com/andreabonatti92/chessboard/ChessBoard.java
@@ -6,6 +6,14 @@ import android.view.View;
 
 import androidx.annotation.NonNull;
 
+/**
+ * The main idea is to use the screen’s length to create the starting square.
+ * The square represents the full board
+ * The next step is divide the board in the different squares
+ * The number of squares is based on a parameter called grid length
+ * The final step will be capture the input of which square will be touch by human finger
+ * I will change the color of the square properly
+ */
 public class ChessBoard extends View {
     public ChessBoard(Context context) {
         super(context);
