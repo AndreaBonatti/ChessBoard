d6fc9f08354ce4063f705b76829e8d19d4889110
Andrea Bonatti <andreabonatti92@gmail.com>
2024-10-09 10:54:13 +0200

    Create the starting point to draw the chess board by using the new class ChessBoard

---
+++ app/src/main/java/com/andreabonatti92/chessboard/ChessBoard.java
@@ -0,0 +1,18 @@
+package com.andreabonatti92.chessboard;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+
+public class ChessBoard extends View {
+    public ChessBoard(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(@NonNull Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
